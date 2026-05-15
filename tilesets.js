558b9a809be5390e98a5f060d496f27ac058a8d5
Spooooky <145117953+Spo0o0ky@users.noreply.github.com>
2026-05-15 15:34:02 +0100

    Fixed animated tiles using / instead of _

--- tilesets.js
+++ tilesets.js
@@ -179,36 +179,39 @@ function getXML(path, sound) {
 }
 
 function getXML_animTiles(path, sound, animTiles) {
+
+    let sprite = animTiles.replace("/","_")
+
     return `<Tileset id="X" path="Spooooky/AssetPack/${path}" sound="${sound}">
-    <set mask="x0x-111-x1x" tiles="6,5; 7,5; 8,5; 9,5" sprites="${animTiles}"/>
+    <set mask="x0x-111-x1x" tiles="6,5; 7,5; 8,5; 9,5" sprites="${sprite}"/>
     <set mask="x1x-111-x0x" tiles="6,10; 7,10; 8,10; 9,10"/>
     <set mask="x1x-011-x1x" tiles="5,6; 5,7; 5,8; 5,9"/>
     <set mask="x1x-110-x1x" tiles="10,6; 10,7; 10,8; 10,9"/>
-    <set mask="x0x-111-110" tiles="1,10" sprites="${animTiles}"/>
-    <set mask="x0x-111-011" tiles="2,10" sprites="${animTiles}"/>
+    <set mask="x0x-111-110" tiles="1,10" sprites="${sprite}"/>
+    <set mask="x0x-111-011" tiles="2,10" sprites="${sprite}"/>
     <set mask="110-111-x0x" tiles="1,11"/>
     <set mask="011-111-x0x" tiles="2,11"/>
     <set mask="x11-011-x10" tiles="10,1"/>
     <set mask="x10-011-x11" tiles="10,2"/>
     <set mask="11x-110-01x" tiles="11,1"/>
     <set mask="01x-110-11x" tiles="11,2"/>
-    <set mask="x0x-111-010" tiles="8,11" sprites="${animTiles}"/>
+    <set mask="x0x-111-010" tiles="8,11" sprites="${sprite}"/>
     <set mask="010-111-x0x" tiles="8,4"/>
     <set mask="01x-110-01x" tiles="4,8"/>
     <set mask="x10-011-x10" tiles="11,8"/>
-    <set mask="x0x-011-x0x" tiles="1,6; 1,7; 1,8; 1,9" sprites="${animTiles}"/>
-    <set mask="x0x-111-x0x" tiles="2,6; 2,7; 2,8; 2,9" sprites="${animTiles}"/>
-    <set mask="x0x-110-x0x" tiles="3,6; 3,7; 3,8; 3,9" sprites="${animTiles}"/>
-    <set mask="x0x-010-x1x" tiles="6,1; 7,1; 8,1; 9,1" sprites="${animTiles}"/>
+    <set mask="x0x-011-x0x" tiles="1,6; 1,7; 1,8; 1,9" sprites="${sprite}"/>
+    <set mask="x0x-111-x0x" tiles="2,6; 2,7; 2,8; 2,9" sprites="${sprite}"/>
+    <set mask="x0x-110-x0x" tiles="3,6; 3,7; 3,8; 3,9" sprites="${sprite}"/>
+    <set mask="x0x-010-x1x" tiles="6,1; 7,1; 8,1; 9,1" sprites="${sprite}"/>
     <set mask="x1x-010-x1x" tiles="6,2; 7,2; 8,2; 9,2"/>
     <set mask="x1x-010-x0x" tiles="6,3; 7,3; 8,3; 9,3"/>
-    <set mask="x0x-010-x0x" tiles="1,1; 2,1; 1,2; 2,2" sprites="${animTiles}"/>
-    <set mask="x0x-011-x1x" tiles="4,4; 5,4; 4,5; 5,5" sprites="${animTiles}"/>
-    <set mask="x0x-110-x1x" tiles="10,4; 11,4; 10,5; 11,5" sprites="${animTiles}"/>
+    <set mask="x0x-010-x0x" tiles="1,1; 2,1; 1,2; 2,2" sprites="${sprite}"/>
+    <set mask="x0x-011-x1x" tiles="4,4; 5,4; 4,5; 5,5" sprites="${sprite}"/>
+    <set mask="x0x-110-x1x" tiles="10,4; 11,4; 10,5; 11,5" sprites="${sprite}"/>
     <set mask="x1x-011-x0x" tiles="4,10; 5,10; 4,11; 5,11"/>
     <set mask="x1x-110-x0x" tiles="10,10; 11,10; 10,11; 11,11"/>
-    <set mask="x0x-011-x10" tiles="6,4" sprites="${animTiles}"/>
-    <set mask="x0x-110-01x" tiles="9,4" sprites="${animTiles}"/>
+    <set mask="x0x-011-x10" tiles="6,4" sprites="${sprite}"/>
+    <set mask="x0x-110-01x" tiles="9,4" sprites="${sprite}"/>
     <set mask="x10-011-x0x" tiles="6,11"/>
     <set mask="01x-110-x0x" tiles="9,11"/>
     <set mask="111-111-110" tiles="1,3"/>
